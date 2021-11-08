bc20a9e59bb5cbc7b08f4203f5eee08f84efc70f
ugodeughi <stefano.corradi@gmail.com>
2021-11-08 13:23:22 +0100

    fine live coding

---
+++ main.js
@@ -0,0 +1,146 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+
+// FUNCTION
+
+const formatDate = (date) => date.split('-').reverse().join('/');
+
+const profileImageTemplate = (userData) => {
+
+    const {name, image} = userData;
+
+    return `<img class="profile-pic" src="${image}" alt="${name}"> `;
+}
+
+const profileDefaultImageTemplate = (userData) =>{
+    /*
+        1. splittare nome e cognome in array
+        2. prendere le iniziali di ogni parola presente in questo array
+        3. concatere le iniziali in una unica stringa
+        4. stampare il template
+     */
+
+    const {name} = userData;
+    const nameParts = name.split(' ');
+    let initials = '';
+    for(let namePart of nameParts){
+        initials += namePart.charAt(0);
+    }
+
+    return `<div class="profile-pic-default">
+                <span>${initials}</span>
+            </div>`;
+
+    
+};
+
+const postTemplate = (post) => {
+
+    const {content, media, author, likes, created} = post;
+ 
+    return `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                    <!-- se l'immagine è presente richiamo la funzione profileImageTemplate altrimenti  profileDefaultImageTemplate -->
+
+                    <!--
+                        if(author.image === true){
+                            profileImageTemplate(author)
+                        }else{
+                            profileDefaultImageTemplate(author)
+                        }
+
+                    -->
+                        ${author.image ? profileImageTemplate(author) : profileDefaultImageTemplate(author)}                 
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${author.name}</div>
+                        <div class="post-meta__time">${formatDate(created)}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${content}</div>
+            <div class="post__image">
+                <img src="${media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `;
+}
+
+
+
+// RENDER DEI POST
+for(let post of posts){
+    document.getElementById('container').innerHTML += postTemplate(post);
+}
